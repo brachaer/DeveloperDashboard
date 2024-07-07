a038c7d9db60dca06ee7e876620056591d8a0c0c
Brachie Erentroy <145331020+brachaer@users.noreply.github.com>
2024-07-07 13:56:02 +0300

    update quotes

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,22 @@ import ProgrammingTip from "./components/ProgrammingTip.jsx";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [quote, setQuote] = createSignal("");
-  const fetchQuote = async () => {
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    setQuote(data.content);
-  };
+  const quotes = [
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "It does not matter how slowly you go as long as you do not stop. - Confucius",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+  ];
 
+  const [quote, setQuote] = createSignal("");
+  const fetchQuote = () => {
+    let min = Math.ceil(0);
+    let max = Math.floor(quotes.length);
+    Math.floor(Math.random() * (max - min)) + min;
+    const randomIndex = Math.floor(Math.random() * (max - min)) + min;
+    setQuote(quotes[randomIndex]);
+  };
   onMount(() => {
     fetchQuote();
   });
